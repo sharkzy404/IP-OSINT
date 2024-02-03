201e69fe192a1e342289f2f33c6eae8fdf542258
Sharkzy404 <ibrababdulshakur2003@gmail.com>
2024-02-04 00:28:18 +0100

    UPDATE

--- ip.py
+++ ip.py
@@ -9,91 +9,85 @@ from os import system as syst
 syst("clear")
 
 def banner():
-    data = """
-                           Mr.SHARK IP OSINT
-                •♪••••••••Powered by ipinfo.io••••••♪••••
+    data = f"""
+                        {Fr.CYAN}Mr.SHARK IP OSINT
+            {Fr.GREEN}•♪••••••••Powered by ipinfo.io••••••♪••••
     """
-    print(Fr.YELLOW+data)
+    print(data)
     St.RESET_ALL
 
-def get_ip_details():
-    print (Fr.BLUE+"[*]To Get device ip info :Y: \n[*]To get quest ip info :N:\n[*]••To clear screen :clear:")
-    print (" ")
-    inp = input(Fr.YELLOW+"[*]: "+St.RESET_ALL).upper()
-    print (" ")
-    if inp == "Y":
-        print (Fr.GREEN+"[*]Loading")
-        tm.sleep(2)
-        num = 0
-        while True:
-            num += 1
-            try:
-                url = requests.get("https://ipinfo.io")
-                break
-            except:
-                print (Fr.RED+"[x]No internet connection", num)
-                tm.sleep(0.5)
-                pass
-    elif inp == "N":
-        while True:
-            try:
-                ip = input(Fr.GREEN+"[*]Guest IP: "+St.RESET_ALL)
-                print (" ")
-                break
-            except:
-                print (Fr.RED+"[x]Bad inputs")
-                pass
-        print (Fr.YELLOW+"[*]Loading\n")
-        tm.sleep(2)
-        num = 0
-        while True:
-            num += 1
-            try:
-                url = requests.post("https://ipinfo.io", json={"ip":ip})
-                break
-            except:
-                print (Fr.RED+"[x]No internet connection", num)
+def device_info():
+    print (Fr.GREEN+"[*]Loading\n")
+    tm.sleep(0.8)
 
-                tm.sleep(0.5)
-                try:
-                    if socket.gethostbyname("facebook.com") == True:
-                        break
-                except socket.gaierror:
-                    pass
-    elif inp == "CLEAR":
-        syst('clear')
-        run_engine()
+    try:
+        url = requests.get("https://ipinfo.io", timeout=5) 
+        data = url.text
+        res = url.status_code
+        if res == 200:
+            load = js.loads(data)
+            keys = load.keys()
 
+            for key in keys:
+                 key_info = key
+                 data_info = load[key_info]
+                 print (Fr.CYAN+"[✓] "+key+": "+Fr.BLUE+str(data_info))
+            print("\n")
+
+        elif res == 404:
+            print(Fr.RED+"[x]Error 404\n")
+            
+    except requests.Timeout:
+        print(Fr.RED+"[x]Error connecting to server\n")
+
+    except requests.exceptions.ConnectionError:
+        print(Fr.RED+"[x]No internet connection \n")
+
+
+
+def guest_info():
+    ip = input(Fr.YELLOW+"[%]Guest ip: "+Fr.WHITE)
+    print (Fr.GREEN+"[*]Loading\n")
+    tm.sleep(0.8)
+
+    try:
+        url = requests.post("https://ipinfo.io", json={"ip":ip}, timeout=5)
+        data = url.text
+        res = url.status_code
+        if res == 200:
+            load = js.loads(data)
+            keys = load.keys()
+
+            for key in keys:
+                key_info = key
+                data_info = load[key_info]
+                print (Fr.CYAN+"[✓] "+key+": "+Fr.BLUE+str(data_info))
+            print("\n")
+
+        elif res == 404:
+            print(Fr.RED+"[x]Error 404: invalid IP\n")
+    
+    except requests.Timeout:
+        print(Fr.RED+"[x)Error connecting to server\n")
+
+    except requests.exceptions.ConnectionError:
+        print(Fr.RED+"[x]No internet connection\n")
+
+
+
+banner()
+while True:
+    print(Fr.CYAN+"[*]To get device ip info input < X >\n[*]To get guest ip info input < Y >\n[*]To clear screen < C >")
+    inp = input(Fr.YELLOW+"[%]: "+Fr.WHITE).upper()
+    if inp == "X":
+        device_info()
+    elif inp == "Y":
+        guest_info()
+    elif inp == "C":
+        syst("clear")
+        banner()
     else:
-        print (Fr.RED+"[x]Invalid")
-        quit(0)
-
-    data = url.text
-    res = url.status_code
-
-    if res == 200:
-        load = js.loads(data)
-        keys = load.keys()
-
-        for key in keys:
-            key_info = key
-            data_info = load[key_info]
-            print (Fr.YELLOW+"[✓] "+key+": "+Fr.GREEN+str(data_info))
-        print (" ")
-        tm.sleep(3)
-
-    elif res == 404:
-        print (Fr.RED+"[x]Error, invalid ip address")
-        print (" ")
-        tm.sleep(3)
+        print(Fr.RED+"[x]Invalid input\n")
 
 
 
-
-
-def run_engine():
-    banner()
-    while True:
-       get_ip_details()
-
-run_engine()
